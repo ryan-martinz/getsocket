d48679e101bc6ddb678e308de61fee804841379d
ryan-martinz <ryan@rocketdollar.com>
2021-08-18 04:08:42 -0500

    Update getsocket.dart

--- lib/getsocket.dart
+++ lib/getsocket.dart
@@ -1,7 +1,7 @@
 library getsocket;
 
-import 'src/html.dart';
+import 'src/html.dart' if (dart.library.io) 'src/io.dart';
 
 class GetSocket extends BaseWebSocket {
-  GetSocket(String url, String path, {Duration ping});
+  GetSocket(String url, {Duration ping}) : super(url, ping: ping);
 }
